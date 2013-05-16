4a41af392c59b38590215989926f839c46f62353
lars <lars@Linux-PC.(none)>
2013-05-16 13:45:32 +0200

    panic

--- src/device/panic.cc
+++ src/device/panic.cc
@@ -10,16 +10,29 @@
 #                    INCLUDES                     #
 \* * * * * * * * * * * * * * * * * * * * * * * * */
 #include "device/panic.h"
+#include "useful/kout.h"
+#include "useful/cpu.h"
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * *\
 #                    METHODS                      # 
 \* * * * * * * * * * * * * * * * * * * * * * * * */
 
+
+
+
+
 /** \todo \~german implementieren \~english write implementation*/
-Panic::Panic(){
+Panic::Panic() : Gate() {
 }
 
 /** \todo \~german implementieren \~english write implementation*/
 void Panic::trigger(){
+
+	kout << "PANIC!!! Interrupt Number: ";	
+	kout << gateInterruptNumber;
+	kout.flush();
+	cpu.disable_int();
+	cpu.halt();
+	
 }
